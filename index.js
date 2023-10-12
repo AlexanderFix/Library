38d9ad83714009c93c9b401e2fe24a2ce3a5f7e3
Александр <reduik@yandex.ru>
2023-10-12 14:50:02 +0300

    update

---
+++ index.js
@@ -0,0 +1,8 @@
+// let nav = document.getElementsByClassName('nav')
+burger_menu.addEventListener('click', () => {
+    burger_menu.classList.toggle('active')
+    burger.classList.toggle('active')
+    nav_ul.classList.toggle('nav_active')
+    nav.classList.toggle('nav_active')
+
+})
